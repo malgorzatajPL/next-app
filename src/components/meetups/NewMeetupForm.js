d3a484ca7e3dcde3506e4c7776584b8ff6c72167
Malgorzata Jaskiewicz <m.jaskiewicz@connexin.co.uk>
2023-07-05 09:31:05 +0100

    Repair broken fetching

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const nameInputRef = useRef();
   const lastNameInputRef = useRef();
   const imageInputRef = useRef();
@@ -20,7 +20,7 @@ function NewMeetupForm() {
       image: enteredImage,
       email: enteredEmail,
     };
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
   return (
     <Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,26 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import { useHistory } from 'react-router-dom';
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://test-62168-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
   return (
     <>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </>
   );
 }
