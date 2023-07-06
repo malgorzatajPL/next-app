39dc1e4092c1e1c3e29dedfee08ea1b7f591e409
Malgorzata Jaskiewicz <m.jaskiewicz@connexin.co.uk>
2023-07-06 11:53:43 +0100

    Add to favorites page logic

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,20 @@
+import MeetupList from '../components/meetups/MeetupList';
+import FavoritesContext from '../store/favorites-context';
+import { useContext } from 'react';
+
 function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+  let content;
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+  return (
+    <section>
+      <h1>Favorite meetups</h1>
+      {content}
+    </section>
+  );
 }
 export default FavoritesPage;
